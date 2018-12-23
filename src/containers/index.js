d822bef093545c42a1d896f3a0556aeb9e687e5b
Jappreet Singh <jappreet.singh@decurtis.com>
2018-12-23 17:13:33 +0530

    redux store setup

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-const App = () => (
-  <React.Fragment>
-    <div>Inshorts News</div>
-  </React.Fragment>
-);
+import { simpleAction } from './../actions/simpleAction';
 
-export default App;
+
+class App extends Component {
+  testAction = () => {
+    this.props.actions.simpleAction();
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div>Inshorts News</div>
+        <button type="button" onClick={this.testAction}>Test Redux Action</button>
+        <pre>
+          {JSON.stringify(this.props)}
+        </pre>
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  state,
+});
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({
+    simpleAction,
+  }, dispatch),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
