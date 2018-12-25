50fe832ac9ad38c0d079f3a463ed40bae1f31bdf
Jappreet Singh <jappreet.singh@decurtis.com>
2018-12-26 00:29:09 +0530

    Header Component Responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import inshortsIcon from './../../global/assets/inshorts.png';
+import './Header.scss';
+
 class Header extends React.Component {
   render() {
     return (
       <React.Fragment>
-        Header
+        <div className="header">
+          <img alt="inshorts" src={inshortsIcon} />
+          inshorts
+        </div>
       </React.Fragment>
     );
   }
